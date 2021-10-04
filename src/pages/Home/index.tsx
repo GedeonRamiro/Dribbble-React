018adb7feaa85378dd649e482cb93c0b6aacd70d
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-04 19:46:38 -0300

    feat:(Home): create style Main

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,20 +2,69 @@
 import { useGlobalState } from "../../context/GlobalContext";
 import { apiWithAuth } from "../../service/api";
 import NavBar from '../../components/NavBar'
+import { useEffect, useState } from "react";
+import { Main } from './style'
+import Card from '../../components/Card'
+
+interface IUserPost{
+  user: {
+    id: string,
+    title: string
+    description: string
+    image_url: string
+    image_id: string
+    created_at: string
+    updated_at: string
+  }
+} 
+
+interface IPosts{
+  id: string,
+  title:string,
+  description: string
+  image_url: string
+  image_id: string
+  user:IUserPost,
+  created_at: string
+  updated_at: string
+}
 
 
 const Home = () => {
 
   const { auth } = useGlobalState()
-  console.log(auth)
+  //console.log(auth)
 
-  apiWithAuth.get('/profile')  
+  const [posts, setPosts] = useState<IPosts[]>([])
+  console.log(posts)
+
+  const getPosts = async () => {
+    const { data } = await apiWithAuth.get<IPosts[]>('/posts')
+    setPosts(data) 
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
 
   return (
-    <div>
-      <NavBar></NavBar>
-      {/* <h1>Usuário: {auth?.user.name}</h1> */}
-    </div>
+    <>
+    <NavBar>
+        <img src={`http://lorempixel.com/400/400/cats/${auth?.user.name}/`} />
+    </NavBar>
+
+    <Main>
+      { posts.map( post => (
+        <Card
+          key={post.id}
+          title={post.title}
+          description={post.description}
+        />
+      ))}
+    </Main>
+    </>
+
   );
 }
 
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+    display: flex;
+    padding: 42px 135px;
+`
