3b3c87eeb7c382efa5fda4db6bd2bbdd4e34844d
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-06 09:21:28 -0300

    feat(Profile): props UserId

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
+import CardProfile from "../../components/CardProfile"
+import { apiWithAuth } from "../../service/api"
+
+interface IProfile{
+  id: string
+  name: string
+  bio: string
+  email: string
+  posts: [
+    {
+      id: string
+      image_url: string,
+
+    }
+  ],
+}
 
 
 interface Istate {
@@ -10,11 +27,29 @@ const Profile = () => {
 
     const { state } = useLocation<Istate>()
     const userId = state.userId
-    console.log(userId)
+    console.log("ID post: ", userId)
+
+    const [profile, setProfile] = useState<IProfile>()
+
+    const getPrtofile = async () => {
+        const { data } = await apiWithAuth.get(`/users/${userId}`)
+        setProfile(data)
+    }
+
+    useEffect(() => {
+        getPrtofile()
+    }, [])
+    
 
 
     return (
-        <h1>{userId}</h1>
+        <CardProfile
+          name={profile?.name}
+          bio={profile?.bio}  
+          posts={profile?.posts}
+          userId={userId}
+        
+        />
     )
 }
 
