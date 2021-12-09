c0829760ca77ff90997852208ccb8f7a5353724c
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-12-09 15:42:55 -0300

    feat: component users

--- src/modules/users/Users/UsersView.tsx
+++ src/modules/users/Users/UsersView.tsx
@@ -13,13 +13,12 @@ const UsersView: React.FC<IProps> = ({users}) => {
 
     return (
         <>
-           
             <div className="grid my-20 gap-y-14 gap-x-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
             {users.map(user => (
                     <div className="relative flex flex-col items-center justify-center pt-20 bg-gray-400 rounded-md shadow-md hover:shadow-lg">
-                        <div className="absolute z-10 p-2 transition bg-pink-500 rounded-full shadow-lg w-28 h-28 -top-8 hover:shadow-xl">
+                        <div className="absolute z-10 p-2 transition bg-gray-100 rounded-full shadow-lg w-28 h-28 -top-8 hover:shadow-xl">
                             <div className="w-full h-full overflow-auto bg-black rounded-full">
-                                <img src={`https://robohash.org/${user.name}`} alt={user.name} />
+                                <img src={`https://robohash.org/${user.name}`} alt={user.name} className='bg-pink-400' />
                             </div>
                         </div>
                         <div className='py-4 tracking-wider text-center'>
@@ -28,7 +27,7 @@ const UsersView: React.FC<IProps> = ({users}) => {
                             <p className="text-gray-100 ">{user.email}</p>
                         </div>
                         <div className='flex flex-wrap content-end w-full '>
-                                <button className='min-w-full p-2 tracking-widest text-white uppercase duration-300 bg-pink-500 rounded-b-md hover:bg-pink-600'>perfil</button>
+                                <button className='min-w-full p-2 tracking-widest text-white uppercase duration-300 bg-gray-500 rounded-b-md hover:bg-pink-500'>perfil</button>
                         </div>
                     </div>
             ))} 
